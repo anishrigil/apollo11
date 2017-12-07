c35362b40342c530356b1dd7af7ecae51f7e2806
Bharata, Anish CTR <Anish.CTR.Bharata@faa.gov>
2017-12-07 13:17:18 -0500

    revised 0

--- src/main/java/com/apollo11/controller/PingController.java
+++ src/main/java/com/apollo11/controller/PingController.java
@@ -21,6 +21,6 @@ public class PingController {
 	@RequestMapping(method = RequestMethod.GET, value = "/revision")
 	ResponseEntity<?> handleRevisionGetRequest(){
 		System.out.println("GetRevisionRequest initiated...");
-		return new ResponseEntity<>("Apollo11 launched revised",HttpStatus.OK);
+		return new ResponseEntity<>("Apollo11 launched revised0",HttpStatus.OK);
 	}
 }
